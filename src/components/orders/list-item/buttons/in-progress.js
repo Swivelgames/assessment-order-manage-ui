dfcf55839772d4dd411973ff199a3755a817dda1
Joseph Dalrymple <swivelgames@gmail.com>
2020-02-23 05:12:46 -0600

    Removed ability to cancel In Progress orders

--- src/components/orders/list-item/buttons/in-progress.js
+++ src/components/orders/list-item/buttons/in-progress.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import ButtonContainer from 'Styled/orders/list-item/right-side';
-import SecondaryButton from 'Styled/button/secondary';
 import BorderedButton from 'Styled/button/bordered';
 
-const InProgressOrderButtons = ({ id, fulfillOrder, cancelOrder }) => {
+const InProgressOrderButtons = ({ id, fulfillOrder }) => {
 	return (
 		<ButtonContainer>
 			<BorderedButton onClick={() => fulfillOrder(id)}>Fulfill</BorderedButton>
-			<SecondaryButton onClick={() => cancelOrder(id)}>Cancel Order</SecondaryButton>
 		</ButtonContainer>
 	);
 };
